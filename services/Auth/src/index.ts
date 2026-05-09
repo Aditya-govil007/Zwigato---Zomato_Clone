6f2387b2c6ba363bb80edf35be9c34755691b0ff
Aditya-govil007 <adityagovil2004@gmail.com>
2026-05-09 15:32:35 +0530

    use cors for OAuth

--- services/Auth/src/index.ts
+++ services/Auth/src/index.ts
@@ -3,12 +3,15 @@ dotenv.config()
 import express from 'express'
 import connectDB from './config/db.js'
 import authRoute from './routes/Auth.js'
+import cors from 'cors'
 
 
 const app=express()
+app.use(cors())
 app.use(express.json());
 
 app.use("/api/auth",authRoute)
+
 
 const PORT= process.env.PORT || 5000
 
