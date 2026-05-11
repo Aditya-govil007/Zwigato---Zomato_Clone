b85d85daf5f9d16a3fd02f7361c0f9e702019a63
Aditya-govil007 <adityagovil2004@gmail.com>
2026-05-11 12:28:35 +0530

    fix the ProtectedRoute

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,22 @@ import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Login from './pages/Login'
 import Home from './pages/Home';
 import {Toaster} from 'react-hot-toast';
+import ProtectedRoute from './components/ProtectedRoute';
+import PublicRoute from './components/PublicRoute';
+import SelectRole from './pages/SelectRole';
 
 const App = () => {
   return (
     <>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/login" element={<Login/>}/>
-      
+      <Route element={<PublicRoute/>}>
+        <Route path="/login" element={<Login/>}/>
+      </Route>
+      <Route element={<ProtectedRoute/>}>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/select-role" element={<SelectRole/>}/>
+      </Route>
     </Routes>
     <Toaster/>
     </BrowserRouter>
